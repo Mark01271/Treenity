ffb715e5eba8e3b0630ed2b96488a99f541ec399
Mark01271 <161707279+Mark01271@users.noreply.github.com>
2024-12-14 14:20:25 +0100

    Creata entità admin

---
+++ TreenityBackend/src/main/java/com/TreenityBackend/entities/Admin.java
@@ -0,0 +1,70 @@
+package com.TreenityBackend.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admins") // Specifica che la tabella del DB si chiama "admins"
+public class Admin {
+
+    @Id
+    @Column(name = "id") // ricorda che è auto increment
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    // getters e setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+    
+    
+}
