f49afb089e95ad4e5a9e03fb3660d10b8a69716d
mattemagosso <matteo.magosso@edu.itspiemonte.it>
2025-01-02 15:47:34 +0100

    Update AdminService.java

--- TreenityBackend/src/main/java/com/TreenityBackend/services/AdminService.java
+++ TreenityBackend/src/main/java/com/TreenityBackend/services/AdminService.java
@@ -30,4 +30,6 @@ public interface AdminService {
 
     // Elimina un admin in base al suo id
     void deleteAdmin(Integer id);
+
+	String changePassword(Integer id, String oldPassword, String newPassword);
 }
