57a49ba61f2e87657ab97e1059251c0a8107e159
Francesco Labate <franco.labate19@gmail.com>
2025-01-12 16:00:59 +0100

    fix carousel

--- TreenityFrontend/src/scripts/home.ts
+++ TreenityFrontend/src/scripts/home.ts
@@ -3,7 +3,16 @@
 //Import home.scss file
 import "/src/styles/home.scss";
 
-import bulmaCarousel from "/bulma-carousel/dist/js/bulma-carousel.min.js";
+import "bulma-carousel/dist/js/bulma-carousel.min.js";
+
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach(".carousel", options);
+
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector("#my-element");
+if (element && element.bulmaCarousel) {
+  // bulmaCarousel instance is available as element.bulmaCarousel
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   // Example: Fetching data from the backend API
