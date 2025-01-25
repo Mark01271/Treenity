e02da406b959d2897bbc7f7b8cfc4c0d1798e230
Francesco Labate <franco.labate19@gmail.com>
2025-01-26 00:33:12 +0100

    Fixed activities page

--- TreenityFrontend/src/scripts/main.ts
+++ TreenityFrontend/src/scripts/main.ts
@@ -1,16 +1,16 @@
-import 'bulma/css/bulma.css'
+import "bulma/css/bulma.css";
 
-import '/src/styles/main.scss'
+import "/src/styles/main.scss";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.navbar-burger');
-  const menu = document.querySelector('.navbar-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".navbar-burger");
+  const menu = document.querySelector(".navbar-menu");
 
   if (burger) {
-    burger.addEventListener('click', () => {
-      burger.classList.toggle('is-active');
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("is-active");
       if (menu) {
-        menu.classList.toggle('is-active');
+        menu.classList.toggle("is-active");
       }
     });
   }
