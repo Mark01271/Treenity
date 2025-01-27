b7518ab8fdf8423726e9603cfd649629bd36fd22
Francesco Labate <franco.labate19@gmail.com>
2025-01-27 12:50:36 +0100

    Aggiunto nuovo menu

--- TreenityFrontend/src/scripts/main.ts
+++ TreenityFrontend/src/scripts/main.ts
@@ -1,17 +1,3 @@
 import "bulma/css/bulma.css";
 
 import "/src/styles/main.scss";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".navbar-burger");
-  const menu = document.querySelector(".navbar-menu");
-
-  if (burger) {
-    burger.addEventListener("click", () => {
-      burger.classList.toggle("is-active");
-      if (menu) {
-        menu.classList.toggle("is-active");
-      }
-    });
-  }
-});
