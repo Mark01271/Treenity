2882d2f23e1a4ec5622af025e11c1c7d0ac0fa67
mattemagosso <matteo.magosso@edu.itspiemonte.it>
2025-01-28 14:18:59 +0100

    created jar

--- TreenityBackend/src/test/java/com/example/demo/TreenityBackendApplicationTests.java
+++ TreenityBackend/src/test/java/com/example/demo/TreenityBackendApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
+import com.TreenityBackend.TreenityBackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TreenityBackendApplication.class)
 class TreenityBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
